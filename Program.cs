ed00d746044bc771d8efb8e82dde39fc52aa3c2c
Adrian Lowdon <adrian.lowdon@softwire.com>
2014-02-13 10:45:06 +0000

    Added logic to handle interrupted connection to traffic lights

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using trafficlight.TrafficLightControls;
+﻿using System;
+using trafficlight.TrafficLightControls;
 
 namespace trafficlight
 {
@@ -6,8 +7,33 @@ namespace trafficlight
     {
         public static void Main(string[] args)
         {
-            ITrafficLightControl control = new Disco(new TrafficLightInterface());
-            control.Activate();
+            DateTime? timeOfLastException = null;
+
+            while (true)
+            {
+                try
+                {
+                    ITrafficLightControl control = new Disco(new TrafficLightInterface());
+                    control.Activate();
+                }
+                catch (Exception e)
+                {
+                    /* 
+                     * Sometimes the connection to the traffic light interface is interrupted, e.g. by power cycling. 
+                     * By catching the exception here, we can try to recreate the TrafficLightInterface and continue,
+                     * rather than having to terminate the program. However, if there's another issue which will cause
+                     * exceptions to be continually thrown, then we should give up and terminate. 
+                     */
+                    var currentDateTime = DateTime.UtcNow;
+
+                    if (timeOfLastException.HasValue && (currentDateTime - timeOfLastException.Value).TotalMinutes < 1)
+                    {
+                        throw e;
+                    }
+
+                    timeOfLastException = currentDateTime;
+                }
+            }
         }
     }
 }
